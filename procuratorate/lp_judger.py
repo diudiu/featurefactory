9e6ac35dfda034b26e0bc313e76cb5f91ebf6e4c
sunjunpeng <sunjunpeng@digcredit.com>
2017-02-10 18:23:53 +0800

    sjp

--- procuratorate/lp_judger.py
+++ procuratorate/lp_judger.py
@@ -50,8 +50,11 @@ class Judger(object):
         self.arguments = apply_base.load()
 
         if not self.arguments:
-            logger.error('Response from the function of `judge._decrypt`, error_msg=%s, rel_err_msg=%s'
-                         % (GetArgumentsError.message, "Missing arguments in the post_data"), exc_info=True)
+            logger.error(
+                'Response from the function of `judge._decrypt`, error_msg=%s, rel_err_msg=%s, apply_id=%s'
+                % (GetArgumentsError.message, "Missing arguments in the post_data", self.apply_id),
+                exc_info=True
+            )
             raise GetArgumentsError  # E06
 
         arg_msg_list = FeatureFieldRel.objects.filter(
@@ -62,7 +65,6 @@ class Judger(object):
             if arg_msg.raw_field_name in self.arguments.keys():
                 if self.ret_msg and (arg_msg.feature_name == (self.ret_msg[-1])['target_field_name']):
                     sub_msg = self.ret_msg[-1]
-
                     if arg_msg.feature_name == sub_msg['target_field_name']:
                         sub_msg['arguments'].update({
                             arg_msg.raw_field_name: self.arguments[arg_msg.raw_field_name],
@@ -78,9 +80,16 @@ class Judger(object):
                     }
                     self.ret_msg.append(temp_msg)
             else:
-                logger.error('Response from the function of `judge._args_useful_check`, error_msg=%s, rel_err_msg=%s'
-                             % (ArgumentsAvailableError.message, "Arguments are not enough to get all res_keys"),
-                             exc_info=True)
+                logger.info(
+                    'Arguments are not enough to get all res_keys, \n filed_name=%s, apply_id=%s'
+                    % (arg_msg.raw_field_name, self.apply_id)
+                )
+                logger.error(
+                    'Response from the function of `judge._args_useful_check`, '
+                    'error_msg=%s, rel_err_msg=%s, apply_id=%s'
+                    % (ArgumentsAvailableError.message, "Arguments are not enough to get all res_keys", self.apply_id),
+                    exc_info=True
+                )
                 raise ArgumentsAvailableError  # E07
 
     def _prepare_args(self):
