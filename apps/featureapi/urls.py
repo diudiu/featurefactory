a39642abe224dff5ab773774030c99275d130486
guoyafei@digcredit.com <guoyafei@digcredit.com>
2017-02-15 17:49:15 +0800

    hh

--- apps/featureapi/urls.py
+++ apps/featureapi/urls.py
@@ -5,11 +5,13 @@
 
 from django.conf.urls import patterns, url
 
+from apps.featureapi.test.unit_api import ProcessDispatch, DataGetDispatch, DataOceanTest
 from .views.generic import FeatureExtract
-from data_test import DataTest
 
 urlpatterns = patterns(
     '',
     url(r'^extract/$', FeatureExtract.as_view(), name='feature_extract'),
-    url(r'^data_test/$', DataTest.as_view(), name='data_test'),
+    url(r'^data_get_dispatch/$', DataGetDispatch.as_view(), name='data_get_dispatch'),
+    url(r'^process_dispatch/$', ProcessDispatch.as_view(), name='process_dispatch'),
+    url(r'^rule/gateway/(?P<data_identity>\w+)/$', DataOceanTest.as_view(), name='dataocean_test'),
 )
