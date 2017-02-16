e052e7171ddc1d5787d225517fd49b959f356c6d
sunjunpeng <sunjunpeng@digcredit.com>
2017-02-16 15:47:18 +0800

    sjp

--- apps/etl/models.py
+++ apps/etl/models.py
@@ -25,9 +25,9 @@ class FeatureShuntConf(BaseModel):
 
     id = models.AutoField(u'主键', primary_key=True)
     feature_name = models.CharField(u'特征字段名', max_length=64)
-    shunt_key = models.CharField(u'', max_length=64)
-    shunt_type = models.CharField(u'', max_length=256)
-    shunt_value = models.CharField(u'', max_length=256)
+    shunt_key = models.CharField(u'分流依据字段名称', max_length=64)
+    shunt_type = models.CharField(u'分流逻辑名', max_length=256)
+    shunt_value = models.CharField(u'分流比较值', max_length=256)
     data_identity = models.CharField(u'原始数据标识', max_length=64)
 
     class Meta:
@@ -37,6 +37,11 @@ class FeatureShuntConf(BaseModel):
 
 
 class FeatureRelevanceConf(BaseModel):
+
+    id = models.AutoField(u'主键', primary_key=True)
+    feature_name = models.CharField(u'特征字段名', max_length=64)
+    depend_feature = models.CharField(u'此特征依赖的其他特征名', max_length=64, null=True)
+    data_identity = models.CharField(u'', max_length=64)
 
     class Meta:
         db_table = 'fic_feature_relevance_conf'
--- slumdog/lp_junkman.py
+++ slumdog/lp_junkman.py
@@ -13,6 +13,7 @@ import requests
 import time
 
 from apps.etl.context import OriginalContext, CacheContext
+from apps.etl.models import FeatureShuntConf
 from vendor.utils.phone_operator_judge import PhoneOperator
 
 logger = logging.getLogger('apps.remote')
@@ -26,10 +27,10 @@ class Courier(object):
         1.缓存数据库(有效期一天?????)
         2.接口数据获取
     """
-    def __init__(self, apply_id, useful_args, interface_data):
+    def __init__(self, apply_id, base_data, interface_data):
         self.apply_id = apply_id
         self.interface_conf = interface_data
-        self.args = {arg['data_identity']: arg['arguments'] for arg in useful_args}
+        self.args = {arg['data_identity']: arg['arguments'] for arg in base_data['useful_args']}
         self.data_identity_list = self.args.keys()
         self.original_base = OriginalContext(self.apply_id)  # MongoDB data
         self.cache_base = CacheContext(self.apply_id)  # MongoDB data
@@ -94,29 +95,27 @@ class ShuntCourier(object):
         2.接口数据获取
     """
 
-    def __init__(self, apply_id, useful_args, interface_data):
+    def __init__(self, apply_id, base_data, interface_data):
         self.apply_id = apply_id
+        self.feature_list = base_data['feature_list']
         self.interface_conf = interface_data
-        self.args = {arg['data_identity']: arg['arguments'] for arg in useful_args}
+        self.args = {arg['data_identity']: arg['arguments'] for arg in base_data['useful_args']}
         self.data_identity_list = self.args.keys()
         self.original_base = OriginalContext(self.apply_id)  # MongoDB data
         self.cache_base = CacheContext(self.apply_id)  # MongoDB data
 
     def work_stream(self):
-        # TODO 分流逻辑 现在这个不好使 重做!
+        # TODO 嗯  这次这个应该好使了
         return self._get_data_by_keys()
 
     def _get_data_by_keys(self):
-        self._get_useful_key()
+        useful_identity_list = self._get_useful_key()
         cache_data = {}
         fresh_data = {}
-        shunt_key = self.args[self.data_identity_list[0]]['mobile']
-        po = PhoneOperator(shunt_key)
-        shunt = po.distinguish()
         for interface in self.interface_conf.iterator():
-            if shunt != interface.comment:
-                continue
             data_identity = interface.data_identity
+            if data_identity not in useful_identity_list:
+                continue
             data = self.cache_base.get(data_identity)
             if data:
                 cache_data.update({
@@ -137,9 +136,29 @@ class ShuntCourier(object):
         return fresh_data
 
     def _get_useful_key(self):
-        data_identity = ''
-
-        return data_identity
+        useful_data_identity = []
+        feature_conf_list = FeatureShuntConf.objects.filter(
+            feature_name__in=self.feature_list,
+            is_delete=False
+        )
+        for feature_conf in feature_conf_list:
+            shunt_key = feature_conf.shunt_key
+            data_identity = feature_conf.data_identity
+            parms = self.args.get(data_identity, None)
+            if not parms:
+                raise
+            value = parms.get(shunt_key, None)
+            if not value:
+                raise
+            oper = feature_conf.shunt_type
+            if oper == 'PhoneOperator':
+                po = PhoneOperator(value)
+                shunt = po.distinguish()
+                if shunt in eval(feature_conf.shunt_value):
+                    useful_data_identity.append(data_identity)
+                else:
+                    continue
+        return list(set(useful_data_identity))
 
     def _get_data_from_interface(self, interface, prams):
         url = interface.data_source.backend_url + interface.route
@@ -176,10 +195,10 @@ class RelevanceCourier(object):
         2.接口数据获取
     """
 
-    def __init__(self, apply_id, useful_args, interface_data):
+    def __init__(self, apply_id, base_data, interface_data):
         self.apply_id = apply_id
         self.interface_conf = interface_data
-        self.args = {arg['data_identity']: arg['arguments'] for arg in useful_args}
+        self.args = {arg['data_identity']: arg['arguments'] for arg in base_data['useful_args']}
         self.data_identity_list = self.args.keys()
         self.original_base = OriginalContext(self.apply_id)  # MongoDB data
         self.cache_base = CacheContext(self.apply_id)  # MongoDB data
@@ -189,10 +208,50 @@ class RelevanceCourier(object):
         return self._get_data_by_keys()
 
     def _get_data_by_keys(self):
-        pass
+        useful_identity_list = self._get_ordered_data_identity()
+        cache_data = {}
+        fresh_data = {}
+        for interface in self.interface_conf.iterator():
+            data_identity = interface.data_identity
+            if data_identity not in useful_identity_list:
+                continue
+            data = self.cache_base.get(data_identity)
+            if data:
+                cache_data.update({
+                    data_identity: data[data_identity]
+                })
+            else:
+                prams = self.args[interface.data_identity]
+                if not prams:
+                    raise
+                data = self._get_data_from_interface(interface, prams)
+                if data:
+                    fresh_data.update(data)
+                    self.cache_base.kwargs.update(data)
+                    self.cache_base.data_identity = data.keys()[0]
+                self.cache_base.save()
+            self.original_base.save()
+        fresh_data.update(cache_data)
+        return fresh_data
+
+    def _get_ordered_data_identity(self):
+        ordered_data_identity = []
+
+        return ordered_data_identity
 
     def _get_data_from_interface(self, interface, prams):
-        pass
+        url = interface.data_source.backend_url + interface.route
+        data_prams = eval(interface.must_data % prams)
+        origin_data = self.do_request(url, data_prams)
+        if not origin_data:
+            raise  # TODO get data error
+        self.original_base.kwargs.update({
+            interface.data_identity: {
+                'origin_data': origin_data,
+                'prams': prams,
+            }
+        })
+        return {interface.data_identity: origin_data}
 
     def do_request(self, url, data):
         pass
