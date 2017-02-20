05472182281e799710208da438a3a38e56e414bf
shniu <niushaohan@digcredit.com>
2017-02-20 13:36:04 +0800

    code desc

--- vendor/errors/feature.py
+++ vendor/errors/feature.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from vendor.errors.common import ServerError
+from vendor.messages.response_code import ResponseCode
 
 
 class FeatureProcessError(ServerError):
-    status = ''
-    message = ''
+    status = ResponseCode.FEATURE_PROCESS_ERROR
+    message = ResponseCode.message(status)
+    log_message = message
+
+    def __init__(self, message=None):
+        if message:
+            self.log_message = message
+
+        super(FeatureProcessError, self).__init__(self.message)
+
+
+
