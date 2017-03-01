7c9c32675e5143aa5a65bec9a8d32c220fccfba8
guoyafei@digcredit.com <guoyafei@digcredit.com>
2017-03-01 09:25:38 +0800

    ff

--- vendor/func_lib/assert_handle.py
+++ vendor/func_lib/assert_handle.py
@@ -4,6 +4,7 @@ from vendor.errors.feature import FeatureProcessError
 
 
 def f_assert_not_null(value_list):
+    """检测值是否非空或值得列表是否存在非空元素"""
     tmp = value_list
     if not isinstance(tmp, list):
         tmp = [tmp]
@@ -14,6 +15,7 @@ def f_assert_not_null(value_list):
 
 
 def f_assert_must_int(value_list):
+    """检测列表中的元素是否为int类型"""
     for value in value_list:
         if not isinstance(value, int):
             raise FeatureProcessError('%s f_assert_must_int Error' % value_list)
@@ -21,6 +23,7 @@ def f_assert_must_int(value_list):
 
 
 def f_assert_must_list(value_list):
+    """检测列表中的元素是否为list类型"""
     for value in value_list:
         if not isinstance(value, list):
             raise FeatureProcessError('%s f_assert_must_list Error' % value_list)
@@ -28,6 +31,7 @@ def f_assert_must_list(value_list):
 
 
 def f_assert_must_dict(value_list):
+    """检测列表中的元素是否为dict类型"""
     for value in value_list:
         if not isinstance(value, dict):
             raise FeatureProcessError('%s f_assert_must_dict Error' % value_list)
@@ -35,6 +39,7 @@ def f_assert_must_dict(value_list):
 
 
 def f_assert_must_digit(value_list):
+    """检测列表中的元素是否为数字"""
     for value in value_list:
         if not str(value).isdigit():
             raise FeatureProcessError('%s f_assert_must_digit Error' % value_list)
@@ -42,6 +47,7 @@ def f_assert_must_digit(value_list):
 
 
 def f_assert_must_basestring(value_list):
+    """检测列表中的元素是否为字符串"""
     for value in value_list:
         if not isinstance(value, basestring):
             raise FeatureProcessError('%s f_assert_must_basestring Error' % value_list)
@@ -49,6 +55,7 @@ def f_assert_must_basestring(value_list):
 
 
 def f_assert_must_digit_or_float(value_list):
+    """检测列表中的元素是否为数字或float"""
     for value in value_list:
         if not (str(value).count('.') <= 1 and str(value).replace('.', '').isdigit()):
             raise FeatureProcessError('%s f_assert_must_digit_or_float Error' % value_list)
@@ -56,6 +63,18 @@ def f_assert_must_digit_or_float(value_list):
 
 
 def f_assert_must_between(value_list, args):
+    """
+    检测列表中的元素是否为数字且在args的范围内
+
+    :param value_list: 待检测列表
+    :param args:        范围列表
+    :return:            异常或原值
+
+    example：
+                :value_list  [2, 2, 3]
+                :args  [1,3]
+    """
+
     assert len(args) == 2
     for value in value_list:
         if not (str(value).isdigit() and int(args[0]) <= int(value) <= int(args[1])):
@@ -64,6 +83,7 @@ def f_assert_must_between(value_list, args):
 
 
 def f_assert_seq0_gte_seq1(value_list):
+    """检测列表中的第一个元素是否大于等于第二个元素"""
     if not value_list[0] >= value_list[1]:
         raise FeatureProcessError('%s f_assert_seq0_gte_seq1 Error' % value_list)
     return value_list
