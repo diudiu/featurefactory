acc7fce928f28a0de03867cdd779e83d4de1a612
sunjunpeng <sunjunpeng@digcredit.com>
2017-03-08 13:49:05 +0800

    sjp

--- studio/feature_comment_handle/yf_config.py
+++ studio/feature_comment_handle/yf_config.py
@@ -304,8 +304,8 @@ config = dict(
 
     mobile_area_city_code_config={
         "feature_name": "mobile_area_city_code",
-        "feature_data_type": "int",
-        "default_value": "PositiveSignedTypeDefault",
+        "feature_data_type": "string",
+        "default_value": "StringTypeDefault",
         "json_path_list": [
             ("mobile_area", "$..content.mobile_area", "f_assert_not_null->f_assert_must_basestring"),
         ],
@@ -316,8 +316,8 @@ config = dict(
 
     gps_city_code_config={
         "feature_name": "mobile_area_city_code",
-        "feature_data_type": "int",
-        "default_value": "PositiveSignedTypeDefault",
+        "feature_data_type": "string",
+        "default_value": "StringTypeDefault",
         "json_path_list": [
             ("mobile_area", "$..addressComponent.city", "f_assert_not_null->f_assert_must_basestring"),
         ],
