4eb2cd47cd88ece81ea5c0998b8f913e1f799327
sunjunpeng <sunjunpeng@digcredit.com>
2017-03-13 14:18:02 +0800

    sjp

--- apps/integration/views/simulation.py
+++ apps/integration/views/simulation.py
@@ -45,7 +45,7 @@ dataocean_url_data = dataocean_url + "/source/queryinfo/"
 dataocean_url_grant = dataocean_url + "/oauth2/token/"
 client_id = "jpWstKTB3bqPLWNoocVncCVVHniytPn6ROHy82Ze"
 client_secret = "kXXNuP6IOMYr5WxhZihef4Ub6QbOFRPglX0WPPZ5D2sAydh3vDZTCvuMGRFAwlJcu3LoqlQuzs60XE72mxlisrZfhW2DGGCjUHJojkPhiouYrjKrkBj357ThuC61qupk"
-des_key = 'sjdguhcphysghcih'
+des_key = "sjdguhcphysghcih"
 
 
 class Simulation(CsrfExemptMixin, View):
@@ -89,7 +89,7 @@ def local_test(req_data, data_identity):
 def remote_test(req_data, data_identity):
     req_data.update({'data_identity': data_identity})
     token = get_token(dataocean_url_grant, client_secret)
-    req_data.update({"access_token": token["access_token"]})
+    req_data.update({'access_token': token['access_token']})
     content = do_request(dataocean_url_data, req_data, des_key)
     content['res_data'] = json.loads(content['res_data'])
     return content
