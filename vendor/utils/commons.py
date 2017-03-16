21bf557f140e43fbfe447429a0d8d436aca09917
sunjunpeng <sunjunpeng@digcredit.com>
2017-03-16 13:49:56 +0800

    sjp

--- vendor/utils/commons.py
+++ vendor/utils/commons.py
@@ -12,8 +12,8 @@ def get_range_map_val(range_list, input_val):
     mapping_val = None
 
     for item in range_list:
-        range = item.get('range')
-        if operator.ge(input_val, range[0]) and operator.le(input_val, range[1]):
+        rangex = item.get('range')
+        if operator.ge(input_val, rangex[0]) and operator.le(input_val, rangex[1]):
             mapping_val = item.get('mapping_val')
             break
 
