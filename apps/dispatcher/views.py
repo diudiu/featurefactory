55163a8e6441821e1379a831b35e4461073f68b4
guoyafei@digcredit.com <guoyafei@digcredit.com>
2017-08-22 11:55:20 +0800

    ff

--- apps/dispatcher/views.py
+++ apps/dispatcher/views.py
@@ -40,10 +40,12 @@ class ApplyCreditView(APIView):
         # 访问风控1.0, 得到相应的apply_id和返回数据
         ret_data_list = rc1.do_request_1_apply(post_data)
         apply_id_1 = ret_data_list[0]
+        logger.info("apply_id_1: %s" % apply_id_1)
         ret_data = ret_data_list[1]
 
         # 访问2.0 得到相应的apply_id
         apply_id_2 = rc2.do_formal_request(data_2_0)
+        logger.info("apply_id_2: %s" % apply_id_2)
         red = RedisX()
 
         # 准备redis数据 包含审核状态和 apply_id映射
@@ -71,13 +73,16 @@ class ObtainCreditResultView(APIView):
         rc1 = RiskControl1()
         # 获取需要获取的申请人apply_id  此时是1.0版本的apply_id
         apply_id = request.query_params.get("apply_id")
+        logger.info("apply_id_1: %s" % apply_id)
         # 拼装返回正在审核中的返回结果
         not_ok_data = {"status": 1, "res_data": {"result": 100, "result_message": "审核中"}, "message": "操作成功"}
         # 取得redis中的状态信息
         redis_status = red.get(apply_id)
         if redis_status:
             redis_status = json.loads(redis_status)
-
+        else:
+            redis_status = {}
+            logger.error("redis_status: %s " % redis_status)
         # 如果redis状态表示审核完成, 调用1.0获取结果接口, 透传返回信息
         if redis_status.get("status") == "OK":
             ret_data = rc1.do_request_1_result(request.query_params)
@@ -99,14 +104,20 @@ class AsyncCallbackView(CsrfExemptMixin, View):
         # 访问风控1.0, 得到相应的apply_id和返回数据
         ret_data = rc1.do_request_1_callback(json.dumps(post_data))
         apply_id_1 = post_data.get("apply_id")
+        logger.info("apply_id_1:%s" % apply_id_1)
 
         red = RedisX()
+
         redis_status = red.get(apply_id_1)
         if redis_status:
             redis_status = json.loads(redis_status)
+        else:
+            redis_status = {}
+            logger.error("redis_status: %s" % redis_status)
 
         # 获取对应2.0的apply_id
         apply_id_2 = redis_status.get("apply_id_2_0")
+        logger.info("apply_id_2:%s" % apply_id_2)
 
         rc2.do_request_2(post_data, apply_id_2)
 
@@ -129,11 +140,17 @@ class ReceiveResult(CsrfExemptMixin, View):
         logger.info("2.0 callback data is: %s" % req_data)
         # 获取2.0的apply_id
         apply_id_2 = req_data.get("apply_id")
+        logger.info("apply_id_2: %s"% apply_id_2)
         # 在redis中查得对应的1.0的apply_id
         apply_id_1 = red.get(apply_id_2)
+        if not apply_id_1:
+            logger.error("apply_id_1 is None!")
+        logger.info("apply_id_1: %s" % apply_id_1)
         # 拿到redis中存储的审核状态
         redis_status_json = red.get(apply_id_1)
         logger.info("receive result redis data is: %s" % redis_status_json)
+        if not redis_status_json:
+            redis_status_json = {}
         redis_status = json.loads(redis_status_json)
         redis_status.update({"status": "OK"})
 
