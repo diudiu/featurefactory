a2f8e91f06c8a75b4a9aa069ee6ce25ebb47ac62
guoyafei@digcredit.com <guoyafei@digcredit.com>
2017-09-12 17:59:00 +0800

    FF

---
+++ apps/dispatcher/settings_deploy.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+import os
+URL_1_0_APPLY = "http://" + os.getenv('RISKCTL_1', '') + "/api/credit/apply/"
+URL_1_0_CALLBACK = "http://" + os.getenv('RISKCTL_1', '') + "/api/async/callback/"
+URL_1_0_RESULT = "http://" + os.getenv('RISKCTL_1', '') + "/api/credit/result/"
+
+
+URL_2_0_FORMAL = "http://" + os.getenv('RISKCTL_2_DE', '') + "/syph-re/api/credit/apply/async/formal/"
+URL_2_0_BANKCARD = "http://" + os.getenv('RISKCTL_2_DE', '') + "/syph-re/api/credit/apply/async/bankcard/"
+URL_2_0_EMAIL = "http://" + os.getenv('RISKCTL_2_DE', '') + "/syph-re/api/credit/apply/async/email/"
+URL_2_0_OPERATOR = "http://" + os.getenv('RISKCTL_2_DE', '') + "/syph-re/api/credit/apply/async/operator/"
+URL_2_0_FINAL = "http://" + os.getenv('RISKCTL_2_DE', '') + "/syph-re/api/credit/apply/async/final/"
+
+
+URL_RECEIVE = "http://" + os.getenv('RISKCTL_2_FF', '') + "/api/async/result/"
+
+HEADERS = {
+    "Content-type": "application/json; charset=utf-8",
+}
